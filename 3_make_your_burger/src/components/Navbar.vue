49b718ed37bb961bdc2f37a37d19b674a7ec539a
viniciusrvcruz <viniciusrobertopro@gmail.com>
2023-09-29 14:48:55 -0300

    API com JSON server e Vue Router

---
+++ 3_make_your_burger/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="nav">
+        <RouterLink to="/">
+            <img src="" alt="logo">
+        </RouterLink>
+        <RouterLink to="/">Home</RouterLink> | 
+        <RouterLink to="/pedidos">Pedidos</RouterLink>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavbarContent"
+}
+</script>
+
+<style>
+</style>
---
+++ 3_make_your_burger/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="home">
+        <h1>HOME</h1>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+
+}
+
+</script>
